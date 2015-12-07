095a62dc52c717f008ae8cc95841b392210425b7
Ben Goertz <goertz@pndlm.com>
2015-12-07 14:42:46 -0800

    Initial Xcode project creation.

---
+++ songfin/songfin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  songfin
+//
+//  Created by Ben Goertz on 12/7/15.
+//  Copyright © 2015 songfininc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
