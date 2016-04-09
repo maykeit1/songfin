74f08dd65f479d2052aec5fae88f8210fb16c5b2
Ben Goertz <bengoertz@gmail.com>
2016-04-08 21:11:22 -0700

    Adding a model for Songs.

---
+++ songfin/songfin/SongModel.swift
@@ -0,0 +1,13 @@
+//
+//  SongModel.swift
+//  songfin
+//
+//  Created by Ben Goertz on 4/8/16.
+//  Copyright © 2016 songfininc. All rights reserved.
+//
+
+import Foundation
+
+struct Song {
+	
+}
