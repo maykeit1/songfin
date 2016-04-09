907a80124a4e8dbbca28a49e6ebb466e5deb7fed
Ben Goertz <bengoertz@gmail.com>
2016-04-08 21:56:15 -0700

    WIP: Starting to build out the model. This is pretty clunky because advanceBy doesn't wrap, so a firstNote of F would crash.

--- songfin/songfin/SongModel.swift
+++ songfin/songfin/SongModel.swift
@@ -8,6 +8,22 @@
 
 import Foundation
 
+enum MusicalNote: Int {
+	case C = 1, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B
+}
+
+typealias Chord = (MusicalNote, MusicalNote, MusicalNote)
+
 struct Song {
+	let key: MusicalNote = .C
 	
+	func getMajorChordFrom(note: MusicalNote) -> Chord {
+		// If the first note is C
+		let firstNote = note
+		// Walk up the scale to the "3", but since we're walking half-steps it's 5 (minus 1 since C is 1)
+		let secondNote = MusicalNote.init(rawValue: firstNote.rawValue.advancedBy(4))
+		let thirdNote = MusicalNote.init(rawValue: firstNote.rawValue.advancedBy(7))
+		
+		return (firstNote, secondNote!, thirdNote!)
+	}
 }
